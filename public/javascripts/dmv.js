d3cc951becc30cfa89f19a8d48ac29579c8e38fe
Rick Waldron waldron.rick@gmail.com <waldron.rick@gmail.com>
2012-03-09 11:44:21 -0500

    Rebuild files
    Signed-off-by: Rick Waldron waldron.rick@gmail.com <waldron.rick@gmail.com>

--- public/javascripts/dmv.js
+++ public/javascripts/dmv.js
@@ -1,4 +1,4 @@
-/*! dmv - v0.2.0 - 3/8/2012
+/*! dmv - v0.2.0 - 3/9/2012
 * https://github.com/rwldrn/dmv
 * Copyright (c) 2012 Rick Waldron <waldron.rick@gmail.com>; Licensed MIT */
 
@@ -41,7 +41,7 @@ var  // Program initializers
     // Store datauri's received from stream
     this.dataUri = "";
 
-    navigator.getUserMedia({ video: true, audio: true }, function( stream ) {
+    navigator.getUserMedia({ video: true, audio: true }, function( raw, stream ) {
     //getUserMedia("video, audio", function( stream ) {
       // Attach user media stream to video container source
       this.media.src = stream;
@@ -156,54 +156,3 @@ var  // Program initializers
 
 
 } (typeof window === "object" && window || this, this.navigator ) );
-
-(function( window, navigator ) {
-  // 2012-03-08 Inspired by https://gist.github.com/f2ac64ed7fc467ccdfe3
-
-  // If unprefix.js is available, use it.
-  // https://github.com/rwldrn/unprefix.js
-  // Otherwise...
-  if ( window.unprefix && window.unprefix.cached ) {
-    // Thanks to Mike Taylr for typing this
-    // https://gist.github.com/f2ac64ed7fc467ccdfe3
-    // normalize window.URL
-    if ( !window.URL ) {
-      window.URL = window.webkitURL || window.msURL || window.oURL;
-    }
-    // normalize navigator.getUserMedia
-    if ( !navigator.getUserMedia ) {
-      navigator.getUserMedia = navigator.webkitGetUserMedia || navigator.mozGetUserMedia || navigator.msGetUserMedia;
-    }
-  }
-
-  var spec = true,
-      getUserMedia = navigator.getUserMedia;
-
-  try {
-    navigator.getUserMedia({ video: true, audio: true }, function() {});
-  } catch(e) {
-    spec = false;
-  }
-
-  navigator.getUserMedia = function( opts, callback, errback ) {
-    var options = opts,
-        // Create guard against bogus options
-        safe = { video: 1, audio: 1 };
-
-    if ( !spec ) {
-      options = Object.keys( opts ).filter(function( key ) {
-        return this[ key ] && safe[ key ];
-      }, opts ).join(",");
-    }
-
-    getUserMedia.call( navigator, options, function( stream ) {
-      //  Standard stream
-      if ( stream.label && stream.readyState === 1 ) {
-        stream = window.URL.createObjectURL( stream );
-      }
-
-      callback( stream );
-    }, errback || function() {});
-  };
-
-} (typeof window === "object" && window || this, this.navigator || {} ) );
