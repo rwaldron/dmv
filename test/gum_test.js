dc5e44bf3352c7cad1eb0df1c6d2dd6e907c931e
Leonardo Balter <leonardo.balter@gmail.com>
2012-03-09 03:32:07 -0300

    only thing I could test in gum.js

--- test/gum_test.js
+++ test/gum_test.js
@@ -1,3 +1,13 @@
 /*global require:true, exports:true */
 
-// ugh.
+var app = require("../lib/gum.js"),
+  navigator = app.navigator;
+
+
+console.log(navigator.getUserMedia);
+
+exports["getUserMedia"] = function(test) {
+  test.expect(1);
+  test.equal(typeof navigator.getUserMedia, "function", "navigator.getUserMedia() is a function");
+  test.done();
+};
