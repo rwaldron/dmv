f333758630c9473de4fe7cec49e6a7cae407bf06
Rick Waldron waldron.rick@gmail.com <waldron.rick@gmail.com>
2012-03-09 14:54:38 -0500

    Update layout: use bootstrap. Rebuild files
    Signed-off-by: Rick Waldron waldron.rick@gmail.com <waldron.rick@gmail.com>

--- public/javascripts/app.js
+++ public/javascripts/app.js
@@ -2,37 +2,40 @@ $(function() {
 
   var id,
       socket = io.connect(),
-      supported = navigator.getUserMedia || navigator.webkitGetUserMedia ||
-                    navigator.mozGetUserMedia || navigator.msGetUserMedia;
+      // Cached DOM Queries
+      $saved = $("#saved");
 
-  if ( $("#container").length ) {
-    // Bye Bye.
-    if ( !supported ) {
-      $("#obsolete").show();
-      return;
-    }
-
-    $("#obsolete").hide();
-
-    // Show Photobooth
-    DMV.init( "#container", socket );
-  } else {
-    // List Photos
-
-    // id initialized at top of scope
-    id = localStorage.getItem("dmv-id");
-
-    socket.emit( "list:request", {
-      id: id
-    }).on( "list:response", function( data ) {
-      if ( data.files.length ) {
-        data.files.forEach(function( file ) {
-          if ( (new RegExp("^" + id )).test(file) ) {
-            // TODO: make some damn templates for this
-            $("#saved").append("<li><a href='" + file + "'><img src='" + file + "'></a></li>");
-          }
-        });
-      }
-    });
+  // Bye Bye.
+  if ( !window.unprefix.supported.getUserMedia ) {
+    $("#obsolete").show();
+    return;
   }
+
+  $("#obsolete").hide();
+
+  // Show Photobooth
+  DMV.init( "#container", socket );
+
+  $("#take-photo").on( "click", DMV.operator.capture.bind( DMV.operator ) );
+
+  // List Photos
+
+  // `id` initialized at top of scope
+  id = localStorage.getItem("dmv-id");
+
+  socket.emit( "list:request", {
+    id: id
+  }).on( "list:response", function( data ) {
+    if ( data.files.length ) {
+      data.files.forEach(function( file ) {
+        if ( (new RegExp( "^" + id )).test(file) ) {
+          // TODO: make some damn templates for this
+          // TODO: be less sloppy about this DOM access
+          $saved.prepend(
+            "<li><a href='/saved/" + file + "' target='_blank'><img src='/saved/" + file + "'></a></li>"
+          );
+        }
+      });
+    }
+  });
 });
